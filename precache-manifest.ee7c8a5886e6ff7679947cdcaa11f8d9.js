9fe9318ac070c1df703799e2911667e5559b7703
Vardges Vardanyan <vako3008@gmail.com>
2020-07-10 18:28:07 +0400

    first commit

---
+++ precache-manifest.ee7c8a5886e6ff7679947cdcaa11f8d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c714664132ef16f3cfbb709c2a9e7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44eec549ac358eff6d93",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bab88192442ef16bc335",
+    "url": "/static/js/2.cf39b50f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf39b50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44eec549ac358eff6d93",
+    "url": "/static/js/main.00063269.chunk.js"
+  },
+  {
+    "revision": "42d070c5aab3c29807eb",
+    "url": "/static/js/runtime-main.d9dc82b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
